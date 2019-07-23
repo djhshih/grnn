b03248214cd6418c1579bfce549347d858c9798b
David J. H. Shih <djh.shih@gmail.com>
2019-07-22 23:00:43 -0500

    Initial commit.
    Still need to fix the two mutable compile error...

---
+++ src/lib.rs
@@ -0,0 +1,205 @@
+extern crate rand;
+extern crate rand_distr;
+
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
+/// Sample from exponential distribution
+fn rexp<R: rand::Rng>(rate: f32, mut rng: &mut R) -> f32 {
+    use rand_distr::{Exp, Distribution};
+    let exp = Exp::new(rate).unwrap();
+    exp.sample(&mut rng)
+}
+
+struct Neuron {
+    /// reserve potential
+    reserve: i16,
+    /// firing rate >= 0
+    rate: f32,
+    /// index of target neurons
+    targets: Vec<usize>,
+    /// probability of outgoing negative signal
+    pnegatives: Vec<f32>,
+    /// probability of outgoing positive signal
+    ppositives: Vec<f32>,
+
+    // Property
+    // sum(pneg) + sum(ppos) + pdissipate == 1
+    // where pdissipate is the probability that the signal dissipates
+}
+
+impl Neuron {
+    /// Receives input.
+    fn receive(&mut self, value: i16) {
+        self.reserve += value;
+        if self.reserve < 0 {
+            self.reserve = 0;
+        }
+    }
+
+    /// Propagates action potential if ready.
+    fn propagate(&mut self, net: &mut Network) {
+        if self.reserve > 0 {
+            use rand_distr::{Uniform, Distribution};
+            let mut rng = rand::thread_rng();
+            let unif = Uniform::new(0.0, 1.0);
+
+            // propagate signal to each target
+            for i in 0..self.targets.len() {
+                let target = &mut net.neurons[self.targets[i]];
+                let pneg = self.pnegatives[i];
+                let ppos = self.ppositives[i];
+
+                let r: f32 = unif.sample(&mut rng);
+                if r < pneg {
+                    target.receive(-1);
+                } else if r < ppos {
+                    target.receive(1);
+                } else {
+                    // output dissipates
+                }
+            }
+            // activation depletes reserve
+            self.reserve -= 1;
+        }
+    }
+}
+
+/// External signal
+struct Signal {
+    /// signal value
+    value: i16,
+    /// arrival rates
+    rate: f32,
+    /// index of target neurons
+    targets: Vec<usize>,
+}
+
+struct Network {
+    /// positive and negative signals
+    signals: Vec<Signal>,
+    /// neurons
+    neurons: Vec<Neuron>,
+    /// priority queue tracking next node to fire
+    queue: Queue
+}
+
+impl Network {
+    fn update(&mut self) {
+        // get the next node to fire
+        // we use unwrap because priority queue must always be populated
+        let node = self.queue.pop().unwrap();
+
+        let rate;
+        if node.index < self.signals.len() {
+            // a signal arrives
+            let signal = &self.signals[node.index];
+            rate = signal.rate;
+            for i in &signal.targets {
+                self.neurons[*i].receive(signal.value);
+            }
+        } else {
+            // a neuron fires
+            let index = node.index - self.signals.len();
+            let neuron = &mut self.neurons[index];
+            rate = neuron.rate;
+            neuron.propagate(self);
+        }
+
+        // push node back onto the queue
+        let mut rng = rand::thread_rng();
+        self.queue.push(
+            Node{ wait: rexp(rate, &mut rng), index: node.index }
+        );
+    }
+}
+
+struct Node {
+    /// wait time until next firing
+    wait: f32,
+    /// index of node;
+    /// order of index of nodes: signals, neurons
+    index: usize,
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.wait == other.wait && self.index == other.index
+    }
+}
+
+impl Eq for Node {}
+
+// prioritize least waiting time
+impl Ord for Node {
+    fn cmp(&self, other: &Node) -> Ordering {
+        // ordering is flipped: other.wait < self.wait
+        // manual implementation requried since cmp is not implemented for f32
+        let ord;
+        if other.wait < self.wait {
+            ord = Ordering::Less;
+        } else if other.wait > self.wait {
+            ord = Ordering::Greater;
+        } else {
+            ord = Ordering::Equal;
+        }
+        
+        // break tie by index to ensure order stability
+        ord.then_with(|| self.index.cmp(&other.index))
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Node) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+/// Priority queue
+/// Determines which node to fire next
+struct Queue {
+    queue: BinaryHeap<Node>
+}
+
+impl Queue {
+    /// Creates a priority queue for a network.
+    fn new(net: &Network) -> Queue {
+        let mut queue = BinaryHeap::new();
+        let mut rng = rand::thread_rng();
+        let mut index: usize = 0;
+
+        for node in &net.signals {
+            queue.push(
+                Node{ wait: rexp(node.rate, &mut rng), index: index }
+            );
+            index += 1;
+        }
+
+        for node in &net.neurons {
+            queue.push(
+                Node{ wait: rexp(node.rate, &mut rng), index: index }
+            );
+            index += 1;
+        }
+
+        Queue{ queue: queue }
+    }
+
+    /// Pushes a node onto the queue while maintaining sorted order.
+    fn push(&mut self, node: Node) {
+        self.queue.push(node)
+    }
+
+    /// Removes next node and return it, or None if queue is empty.
+    fn pop(&mut self) -> Option<Node> {
+        self.queue.pop()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
